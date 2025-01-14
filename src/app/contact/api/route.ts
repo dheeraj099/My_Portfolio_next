40027134d45b24aa5a8acc499008f665cba96663
dheeraj099 <dheerajpgirish@gmail.com>
2025-01-14 13:37:17 +0530

    feat:favicon fixes-2

--- src/app/contact/api/route.ts
+++ src/app/contact/api/route.ts
@@ -1,49 +1,49 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Resend } from "resend";
-import { EmailTemplate } from "@/app/contact/EmailTemplate";
+// import { NextRequest, NextResponse } from "next/server";
+// import { Resend } from "resend";
+// import { EmailTemplate } from "@/app/contact/EmailTemplate";
 
-export async function POST(req: NextRequest) {
-  const body = await req.json();
-  // body of our request - to be sent from the Client-side in our form above
-  const { message, email ,name} = body;
+// export async function POST(req: NextRequest) {
+//   const body = await req.json();
+//   // body of our request - to be sent from the Client-side in our form above
+//   const { message, email ,name} = body;
 
-  // Check if the required fields are present
-  if (!message || !email) {
-    return NextResponse.json(
-      { error: "Missing required fields" },
-      { status: 400 }
-    );
-  }
+//   // Check if the required fields are present
+//   if (!message || !email) {
+//     return NextResponse.json(
+//       { error: "Missing required fields" },
+//       { status: 400 }
+//     );
+//   }
 
-  // resend declaration with API key as parameter
-  const resend = new Resend("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+//   // resend declaration with API key as parameter
+//   const resend = new Resend("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
 
-  try {
-    // resend function handler for executing email sending
-    // returning data and error state to indicate success and failure respecfully
-    const { data, error } = await resend.emails.send({
-      from: `dheerajpgirish.me`, //Title of our Email, here, our email will indicate Imam - Portfolio and the <info@eimaam.dev> will be the sending address. NB: `eimaam.dev` replace with your registered domain
-      to: "dherajpgirish09@gmail.com", // email receiver, // in case where you are sending onboarding emails, this field will be dynamic, it will be the email of the User
-      subject: "Message from Portfolio",
-      react: EmailTemplate({ name ,email, message}), //using our custom react component to render email content/body
-    });
+//   try {
+//     // resend function handler for executing email sending
+//     // returning data and error state to indicate success and failure respecfully
+//     const { data, error } = await resend.emails.send({
+//       from: `dheerajpgirish.me`, //Title of our Email, here, our email will indicate Imam - Portfolio and the <info@eimaam.dev> will be the sending address. NB: `eimaam.dev` replace with your registered domain
+//       to: "dherajpgirish09@gmail.com", // email receiver, // in case where you are sending onboarding emails, this field will be dynamic, it will be the email of the User
+//       subject: "Message from Portfolio",
+//       react: EmailTemplate({ name ,email, message}), //using our custom react component to render email content/body
+//     });
 
-    if (error) {
-      return NextResponse.json(
-        { message: "Email sending failed", error },
-        { status: 400 }
-      );
-    }
+//     if (error) {
+//       return NextResponse.json(
+//         { message: "Email sending failed", error },
+//         { status: 400 }
+//       );
+//     }
 
-    return NextResponse.json(
-      { message: "Email sent successfully", data },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error("Error sending email:", error);
-    return NextResponse.json(
-      { message: "Failed to send email", error },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(
+//       { message: "Email sent successfully", data },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//     return NextResponse.json(
+//       { message: "Failed to send email", error },
+//       { status: 500 }
+//     );
+//   }
+// }
